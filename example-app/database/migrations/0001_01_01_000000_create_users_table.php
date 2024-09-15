88c326ddd7bfa1b209a51b5a708353a714c5d9ec
Danil <dach.king@yandex.ru>
2024-09-16 00:15:36 +0500

    Creating first user

--- example-app/database/migrations/0001_01_01_000000_create_users_table.php
+++ example-app/database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,6 +21,15 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        // Создание начального пользователя
+        DB::table('users')->insert([
+           'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
