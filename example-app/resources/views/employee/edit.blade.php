2a5ceac7e3da6a860144cb345c05d806ddeca2b2
Danil <dach.king@yandex.ru>
2024-09-16 18:33:58 +0500

    Edit Employee

---
+++ example-app/resources/views/employee/edit.blade.php
@@ -0,0 +1,64 @@
+<head>
+    <title>Edit employee: {{ $employee->first_name}} {{ $employee->last_name}}</title>
+</head>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Edit employee') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg max-w-xl">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="form-group">
+                        <form action="" method="post">
+                            @csrf
+                            <div class="form-group mb-2">
+                                <label for="first_name">First Name</label>
+                                <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" id="first_name" value="{{ $employee->first_name }}">
+                                @error('first_name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="last_name">Last Name</label>
+                                <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" id="last_name" value="{{ $employee->last_name }}">
+                                @error('last_name')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="company">Company</label>
+                                <input type="text" class="form-control @error('company') is-invalid @enderror" name="company" id="company" value="{{ $employee->company }}">
+                                @error('company')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-2">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" value="{{ $employee->email }}">
+                                @error('email')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group mb-1">
+                                <label for="phone_number">Phone Number</label>
+                                <input type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" id="phone_number" maxlength="11" value="{{ $employee->phone_number }}">
+                                @error('phone_number')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <button type="submit" class="btn btn-primary mt-2 w-100">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
