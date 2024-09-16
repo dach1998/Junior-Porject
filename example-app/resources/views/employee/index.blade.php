e989d1fb14052b4a443643fcb4253559ad2237a6
Danil <dach.king@yandex.ru>
2024-09-16 16:48:42 +0500

    Added button create employee

--- example-app/resources/views/employee/index.blade.php
+++ example-app/resources/views/employee/index.blade.php
@@ -8,6 +8,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="flex justify-between mb-4">
+                        <h3 class="text-lg font-medium text-gray-900 dark:text-gray-100">Employees</h3>
+                        <a href="{{ route('employee.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Create Employee
+                        </a>
+                    </div>
                     <table class="table-auto w-full text-left whitespace-no-wrap">
                         <thead>
                         <tr>
@@ -32,6 +38,9 @@
                         @endforeach
                         </tbody>
                     </table>
+                    <div class="p-6">
+                        {{ $employees->links() }}
+                    </div>
                 </div>
             </div>
         </div>
